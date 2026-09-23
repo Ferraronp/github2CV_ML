910b3e7f3dcb350f73619f30c8c5f0a90f71e01e
Ferraronp <74415885+Ferraronp@users.noreply.github.com>
2026-09-23 18:36:32 +0300

    Bootstrap Python project structure and tooling (#22)
    Closes #21

---
+++ src/github2cv_ml/__init__.py
@@ -0,0 +1,5 @@
+"""github2CV ML package."""
+
+__version__ = "0.1.0"
+
+__all__ = ["__version__"]
---
+++ tests/test_package.py
@@ -0,0 +1,5 @@
+import github2cv_ml
+
+
+def test_package_imports() -> None:
+    assert github2cv_ml.__version__ == "0.1.0"
